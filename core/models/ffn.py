c6d15e0702496bdfaf71ce18234b0eecead871e9
ChrisXtx <55004840+ChrisXtx@users.noreply.github.com>
2020-04-17 02:18:29 +0800

    Update ffn.py

--- core/models/ffn.py
+++ core/models/ffn.py
@@ -3,32 +3,6 @@ __all__ = ['FFN']
 import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
-import torch
-
-import torch
-import torch.nn as nn
-
-class FilterResponseNormalization(nn.Module):
-    def __init__(self, beta, gamma, tau, eps=1e-6):
-        super(FilterResponseNormalization, self).__init__()
-        self.beta = beta
-        self.gamma = gamma
-        self.tau = tau
-        self.eps = torch.Tensor([eps])
-
-    def forward(self, x):
-
-        n, c, h, w = x.shape
-        assert (self.gamma.shape[1], self.beta.shape[1], self.tau.shape[1]) == (c, c, c)
-        nu2 = torch.mean(x.pow(2), (2,3), keepdims=True)
-        x = x * torch.rsqrt(nu2 + torch.abs(self.eps))
-        return torch.max(self.gamma*x + self.beta, self.tau)
-
-
-
-
-
-
 
 
 class ResBlock(nn.Module):
