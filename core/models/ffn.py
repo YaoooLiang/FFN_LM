7de49f046493ba170d42888f4072adf8e9dc8cdb
ChrisXtx <55004840+ChrisXtx@users.noreply.github.com>
2020-05-08 17:38:12 +0800

    Update ffn.py

--- core/models/ffn.py
+++ core/models/ffn.py
@@ -6,18 +6,20 @@ import torch.nn.functional as F
 
 
 class ResBlock(nn.Module):
-    def __init__(self, in_channels=32, mid_channels=32, kernel_size=(3, 3, 3), padding=1):
+    def __init__(self, in_channels=32, mid_channels=32, out_channels=32, kernel_size=(3, 3, 3), padding=1):
         super(ResBlock, self).__init__()
         self.conv0 = nn.Conv3d(in_channels, mid_channels, kernel_size, padding=padding)
-        self.bn3d = nn.BatchNorm3d(mid_channels)
-        self.conv1 = nn.Conv3d(in_channels, mid_channels, kernel_size, padding=padding)
+        self.gn0 = nn.GroupNorm(int(mid_channels/2),mid_channels)
+        self.conv1 = nn.Conv3d(mid_channels, out_channels, kernel_size, padding=padding)
+        self.gn1 = nn.GroupNorm(int(out_channels/2),out_channels)
 
     def forward(self, x):
-        conv0_out = self.conv0(F.relu(x))
-        BNout = self.bn3d(conv0_out)
-        conv1_out = self.conv1(F.relu(BNout))
+        conv0_out = self.conv0(F.relu(x, inplace=True))
+        gn0_out = self.gn0(conv0_out)
+        conv1_out = self.conv1(F.relu(gn0_out, inplace=True))
+        gn1_out = self.gn1(conv1_out)
 
-        return conv1_out + x
+        return gn1_out + x
 
 
 class FFN(nn.Module):
@@ -26,8 +28,10 @@ class FFN(nn.Module):
         super(FFN, self).__init__()
 
         self.conv0 = nn.Conv3d(in_channels, mid_channels, kernel_size, padding=padding)
+        self.gn0 = nn.GroupNorm(int(mid_channels/2),mid_channels)
         self.conv1 = nn.Conv3d(mid_channels, mid_channels, kernel_size, padding=padding)
-        self.resblocks = nn.Sequential(*[ResBlock(mid_channels, mid_channels, kernel_size, padding) for i in range(1, depth)])
+        self.gn1 = nn.GroupNorm(int(mid_channels/2),mid_channels)
+        self.resblocks = nn.Sequential(*[ResBlock(mid_channels, mid_channels, mid_channels, kernel_size, padding) for i in range(1, depth)])
         self.conv3 = nn.Conv3d(mid_channels, out_channels, (1, 1, 1))
 
         self.input_size = np.array(input_size)
@@ -38,9 +42,11 @@ class FFN(nn.Module):
 
     def forward(self, x):
         conv0_out = self.conv0(x)
-        conv1_out = self.conv1(F.relu(conv0_out))
-        res_out = self.resblocks(conv1_out)
-        logits = self.conv3(F.relu(res_out))
+        gn0_out = self.gn0(conv0_out)
+        conv1_out = self.conv1(F.relu(gn0_out, inplace=True))
+        gn1_out = self.gn1(conv1_out)
+        res_out = self.resblocks(gn1_out)
+        logits = self.conv3(F.relu(res_out, inplace=True))
 
         return logits
 
