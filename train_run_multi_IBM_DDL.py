737710bb3323e9bf7ae0d32b76fa66b5f7b2cdf6
ChrisXtx <55004840+ChrisXtx@users.noreply.github.com>
2020-05-26 14:26:21 +0800

    Update train_run_multi_IBM_DDL.py

--- train_run_multi_IBM_DDL.py
+++ train_run_multi_IBM_DDL.py
@@ -36,8 +36,8 @@ parser.add_argument('--depth', type=int, default=12, help='depth of ffn')
 parser.add_argument('--delta', default=(15, 15, 15), help='delta offset')
 parser.add_argument('--input_size', default=(51, 51, 51), help ='input size')
 
-parser.add_argument('--resume', type=str, default='/home/xiaotx/2017EXBB/model/ffn_model_fov:51_delta:15_depth:12.pth', help='resume training')
-parser.add_argument('--save_path', type=str, default='/home/xiaotx/2017EXBB/model', help='model save path')
+parser.add_argument('--resume', type=str, default='/home/xiaotx/2017EXBB/model/model_fov51_delta15_dep12/ffn_model_fov:51_delta:15_depth:12.pth', help='resume training')
+parser.add_argument('--save_path', type=str, default='/home/xiaotx/2017EXBB/model/model_fov51_delta15_dep12/', help='model save path')
 parser.add_argument('--save_interval', type=str, default=1000, help='model save interval')
 parser.add_argument('--log_save_path', type=str, default='/home/xiaotx/2017EXBB/model/model_log/', help='model_log save path')
 
